cc16628545ffe39e51ffdb395668d516eb46b4e2
rbvancleave <48543007+rbvancleave@users.noreply.github.com>
2021-04-13 17:05:36 -0500

    api keys

--- VacationPy/api_keys.py
+++ VacationPy/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE!"
+weather_api_key = "8b74cc9d666a39e91d6be682ded9f04a"
 
 # Google API Key
-g_key = "YOUR KEY HERE!"
+g_key = "AIzaSyDHnu3sj74UTDufNh3w-cHuAIdT143SeOU"
